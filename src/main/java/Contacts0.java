6ab4837ba0f90db5a742bcb5a11c95f0c1e1d18b
xiaoge26 <liuxiaoge0206@gmail.com>
2023-01-27 17:46:13 +0800

    Refactor code.

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    /**
+     *
+     */
+    private static final int MAX = 100;
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        showWelcomeMessage();
+        String[][] list = new String[MAX][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
@@ -144,6 +145,14 @@ public class Contacts0 {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+    private static void showWelcomeMessage() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 0.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
